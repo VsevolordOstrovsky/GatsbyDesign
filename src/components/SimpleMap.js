f830f5250ba17bad94cd8ece0865c2549a4455fe
anaz <andrexnazarov@gmail.com>
2026-04-21 20:03:25 +0300

    Добавил линии из geojson карты (ее надо переконфертировать), Добавил метки (без класторизации (она у не работает так как в документации)), Поправил README.md

--- src/components/SimpleMap.js
+++ src/components/SimpleMap.js
@@ -21,6 +21,9 @@ export function SimpleMap() {
         hash: true,
       })
 
+      // Сохраняем ссылку на map в локальную переменную
+      mapRef.current = map
+
       // Отображение линий
       map.on("load", function () {
         map.addLayer({
@@ -28,7 +31,7 @@ export function SimpleMap() {
           type: "line",
           source: {
             type: "geojson",
-            data: userLineData,
+            data: "/map.geojson",
           },
           layout: {
             "line-join": "round",
@@ -36,63 +39,53 @@ export function SimpleMap() {
           },
           paint: {
             "line-color": "#AE3478",
-            "line-width": 8,
+            "line-width": 5,
           },
         })
       })
 
-      const coordinates = [37.62, 55.74]
+      const text = "asdasdasdad"
 
-      var marker = new mmrgl.Marker().setLngLat(coordinates).setPopup(
-        new mmrgl.Popup().setHTML(`
-          <div style="text-align: center;">
-            <img 
-                src="https://urcomsod.ru/assets/components/phpthumbof/cache/213.a4f2d4a453b8bedd2fd093f12726df4d.png" 
-                alt="Подстанция"
-                style="width: 100%; max-width: 200px; border-radius: 8px;  margin: auto;"
-            />
-            <hr style="margin: 2px 0; border: none; border-top: 1px solid #ccc;" />
-            <div style="font-size: 12px; line-height: 1.5; text-align: left;">
-                <strong>Тип сооружения:</strong> Электрическая подстанция<br />
-                <strong>Название:</strong> ПС "Центральная"<br />
-                <strong>Напряжение:</strong> 110 кВ<br />
-                <strong>Статус:</strong> Работает<br />
-                <strong>Описание:</strong> Обеспечивает электроэнергией центральный район
-            </div>
-            <hr style="margin: 10px 0; border: none; border-top: 1px solid #ccc;" />
-            <div style="font-size: 12px; color: #666;">
-                📍 Координаты: ${coordinates[0]}, ${coordinates[1]}
-            </div>
-        </div>
-        `)
-      )
-
-      // Сохраняем ссылку на map в локальную переменную
-      mapRef.current = map
-
-      // Отображение точек
-      map.on("load", function () {
-        marker.addTo(map)
-        map.loadImage(
-          "https://maps.vk.com/api/styles/pins/blue_target.png",
-          function (error, image) {
-            if (error) throw error
-            map.addImage("custom_pin", image)
-            map.addLayer({
-              id: "points",
-              type: "symbol",
-              source: {
-                type: "geojson",
-                data: userPointData,
-              },
-              layout: {
-                "icon-image": "custom_pin",
-                "icon-size": 1,
-              },
+      // Отображаем точки на карте БЕЗ КЛАСТЕРИЗАЦИИ
+      map.on("load", () =>  {
+        fetch("/map.geojson")
+          .then(response => response.json())
+          .then(data => {
+            // Просто добавляем маркеры, без кластеризации
+            data.features.filter(object => object.geometry?.type === "Point").forEach(feature => {
+              const [lng, lat] = feature.geometry.coordinates
+              new mmrgl.Marker()
+                .setLngLat([lng, lat])
+                .setPopup(
+                  new mmrgl.Popup().setHTML(`
+                    <div style="text-align: center;">
+                      <img 
+                          src="https://urcomsod.ru/assets/components/phpthumbof/cache/213.a4f2d4a453b8bedd2fd093f12726df4d.png" 
+                          alt="Подстанция"
+                          style="width: 100%; max-width: 200px; border-radius: 8px;  margin: auto;"
+                      />
+                      <hr style="margin: 2px 0; border: none; border-top: 1px solid #ccc;" />
+                      <div style="font-size: 12px; line-height: 1.5; text-align: left;">
+                          <strong>Тип сооружения:</strong> Электрическая подстанция<br />
+                          <strong>Название:</strong> ${feature.properties.name} <br />
+                          <strong>Статус:</strong> Работает<br />
+                          <strong>Описание:</strong> Обеспечивает электроэнергией центральный район
+                      </div>
+                      <hr style="margin: 10px 0; border: none; border-top: 1px solid #ccc;" />
+                      <div style="font-size: 12px; color: #666;">
+                          📍 Координаты: ${feature.geometry.coordinates[0]}, ${feature.geometry.coordinates[0]}
+                      </div>
+                    </div>
+                  `)
+                )
+                .addTo(map)
             })
-          }
-        )
+
+            console.log(`Добавлено ${data.features.length} маркеров`)
+          })
       })
+
+      map.addControl(new mmrgl.NavigationControl(), "top-left");
 
       // Инициализируем контрол измерения расстояния
       distanceControlRef.current = new DistanceControl(map)
@@ -109,7 +102,7 @@ export function SimpleMap() {
         if (currentMap) {
           currentMap.remove()
         }
-      }
+      };
     }
   }, [])
 
