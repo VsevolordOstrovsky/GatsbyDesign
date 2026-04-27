d1fa9686edf24cae26987cf067ac183580a95d60
VsevolordOstrovsky <117922778+VsevolordOstrovsky@users.noreply.github.com>
2026-04-27 10:05:03 +0300

    Revert "Map vk"

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { SimpleMap } from '../components/SimpleMap'; // named import с фигурными скобками
+import React from "react"
+import loadable from "@loadable/component"
 
-export default function MapPage() {
+const SimpleMap = loadable(() => import("../components/SimpleMap"), {
+  ssr: false,
+  fallback: <div style={{ padding: "2rem" }}>Загрузка карты...</div>,
+})
+
+const MapPage = () => {
   return (
-    <div>
+    <div style={{ margin: 0, padding: 0 }}>
       <SimpleMap />
     </div>
-  );
+  )
 }
+
+export default MapPage
