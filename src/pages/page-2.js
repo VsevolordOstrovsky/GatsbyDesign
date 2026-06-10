ec51504ffad0eccf00f16bcb123ff0d612fcaa27
VsevolordOstrovsky <117922778+VsevolordOstrovsky@users.noreply.github.com>
2026-06-10 18:40:16 +0300

    Delete src directory

--- src/pages/page-2.js
+++
@@ -1,5 +0,0 @@
-import Concept1Page, { Head } from "./concept-1"
-
-export { Head }
-
-export default Concept1Page
--- src/pages/using-typescript.js
+++
@@ -1,5 +0,0 @@
-import Concept2Page, { Head } from "./concept-2"
-
-export { Head }
-
-export default Concept2Page
